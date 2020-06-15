4295bfb622dcac0d52f7bc1a43084206068b1509
tuku <utkuyaman@gmail.com>
2020-06-16 02:28:09 +0300

    cors setup

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const dbConfig = require('./config/database.config.js');
+const logger = require('./app/util/logger');
 
 // create express app
 const app = express();
 
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse requests of content-type - application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5000');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 
 // Configuring the database
-const dbConfig = require('./config/database.config.js');
-const mongoose = require('mongoose');
-const logger = require('./app/util/logger');
-
 mongoose.Promise = global.Promise;
 
 // Connecting to the database
@@ -23,15 +29,15 @@ mongoose.connect(dbConfig.url, {
     useUnifiedTopology: dbConfig.useUnifiedTopology,
     autoIndex: dbConfig.autoIndex
 }).then(() => {
-    logger.info("Successfully connected to the database");
-}).catch(err => {
+    logger.info('Successfully connected to the database');
+}).catch((err) => {
     console.log('Could not connect to the database. Exiting now...', err);
     process.exit();
 });
 
 // define a simple route
 app.get('/', (req, res) => {
-    res.json({"message": "Welcome to Kuato backend application."});
+    res.json({ message: 'Welcome to Kuato backend application.' });
 });
 
 require('./app/routes/job.routes.js')(app);
@@ -41,5 +47,5 @@ require('./app/routes/report.routes.js')(app);
 
 // listen for requests
 app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
+    console.log('Server is listening on port 3000');
 });
--- app/controllers/job.controller.js
+++ app/controllers/job.controller.js
@@ -6,52 +6,52 @@ exports.create = (req, res) => {
     // todo Validate request
 
     const job = new Job({
-        name: req.body.name || "Unnamed Job",
+        name: req.body.name || 'Unnamed Job',
         isInstant: req.body.isInstant
     });
 
     job.save()
-        .then(data => {
+        .then((data) => {
             res.send(data);
         })
-        .catch(err => {
+        .catch((err) => {
             res.status(500).send({
-                message: err.message || "Some error occurred while creating the Job."
+                message: err.message || 'Some error occurred while creating the Job.'
             });
         });
 };
 
 exports.findAll = (req, res) => {
     Job.find()
-        .then(jobs => {
+        .then((jobs) => {
             res.send(jobs);
-        }).catch(err => {
-        res.status(500).send({
-            message: err.message || "Some error occurred while retrieving jobs."
+        }).catch((err) => {
+            res.status(500).send({
+                message: err.message || 'Some error occurred while retrieving jobs.'
+            });
         });
-    });
 };
 
 exports.findOne = (req, res) => {
-    let jobId = req.params.jobId;
-    logger.info('jobs; findOne #' + jobId);
+    const { jobId } = req.params;
+    logger.info(`jobs; findOne #${jobId}`);
     Job.findById(jobId)
-        .then(job => {
+        .then((job) => {
             if (!job) {
                 return res.status(404).send({
-                    message: "Job not found with id " + req.params.jobId
+                    message: `Job not found with id ${req.params.jobId}`
                 });
             }
             res.send(job);
         })
-        .catch(err => {
+        .catch((err) => {
             if (err.kind === 'ObjectId') {
                 return res.status(404).send({
-                    message: "Job not found with id " + req.params.jobId
+                    message: `Job not found with id ${req.params.jobId}`
                 });
             }
             return res.status(500).send({
-                message: "Error retrieving job with id " + req.params.jobId
+                message: `Error retrieving job with id ${req.params.jobId}`
             });
         });
 };
@@ -62,81 +62,65 @@ exports.update = (req, res) => {
 
 exports.delete = (req, res) => {
     Job.findByIdAndRemove(req.params.jobId)
-        .then(job => {
+        .then((job) => {
             if (!job) {
                 return res.status(404).send({
-                    message: "Job not found with id " + req.params.jobId
+                    message: `Job not found with id ${req.params.jobId}`
                 });
             }
-            res.send({message: "Job deleted successfully!"});
-        }).catch(err => {
-        if (err.kind === 'ObjectId' || err.name === 'NotFound') {
-            return res.status(404).send({
-                message: "Job not found with id " + req.params.jobId
+            res.send({ message: 'Job deleted successfully!' });
+        }).catch((err) => {
+            if (err.kind === 'ObjectId' || err.name === 'NotFound') {
+                return res.status(404).send({
+                    message: `Job not found with id ${req.params.jobId}`
+                });
+            }
+            return res.status(500).send({
+                message: `Could not delete job with id ${req.params.jobId}`
             });
-        }
-        return res.status(500).send({
-            message: "Could not delete job with id " + req.params.jobId
         });
-    });
 };
 
 exports.addUrl = (req, res) => {
     // todo Validate request
-    let jobId = req.params.jobId;
-    let url = req.body.url;
+    const { jobId } = req.params;
+    const { url } = req.body;
 
-    logger.info('jobs; addUrl #' + jobId);
+    logger.info(`jobs; addUrl #${jobId}`);
     services.job.addUrlToJob(jobId, url)
-        .then(job => {
-            return res.status(200).send(job);
-        })
-        .catch(reason => {
-            return res.status(reason.status).send({message: reason.message});
-        });
+        .then((job) => res.status(200).send(job))
+        .catch((reason) => res.status(reason.status).send({ message: reason.message }));
 };
 
 exports.deleteUrl = (req, res) => {
     // todo Validate request
-    let jobId = req.params.jobId;
-    let url = req.body.url;
+    const { jobId } = req.params;
+    const { url } = req.body;
 
-    logger.info('jobs; deleteUrl #' + jobId);
+    logger.info(`jobs; deleteUrl #${jobId}`);
     services.job.deleteUrlFromJob(jobId, url)
-        .then(job => {
-            return res.status(200).send(job);
-        })
-        .catch(reason => {
-            return res.status(reason.status).send({message: reason.message});
-        });
+        .then((job) => res.status(200).send(job))
+        .catch((reason) => res.status(reason.status).send({ message: reason.message }));
 };
 
 exports.addSearchQuery = (req, res) => {
     // todo Validate request
-    let jobId = req.params.jobId;
-    let searchQueryData = req.body;
+    const { jobId } = req.params;
+    const searchQueryData = req.body;
 
-    logger.info('jobs; addSearchQuery #' + jobId);
+    logger.info(`jobs; addSearchQuery #${jobId}`);
     services.job.addSearchQueryToJob(jobId, searchQueryData, req.data.auth)
-        .then(job => {
-            return res.status(200).send(job);
-        })
-        .catch(reason => {
-            return res.status(reason.status).send({message: reason.message});
-        });
+        .then((job) => res.status(200).send(job))
+        .catch((reason) => res.status(reason.status).send({ message: reason.message }));
 };
 
 exports.deleteSearchQuery = (req, res) => {
     // todo Validate request
-    let jobId = req.params.jobId;
-    let searchQueryId = req.body.searchQueryId;
+    const { jobId } = req.params;
+    const { searchQueryId } = req.body;
 
-    logger.info('jobs; addSearchQuery #' + jobId);
+    logger.info(`jobs; addSearchQuery #${jobId}`);
     services.job.deleteSearchQueryFromJob(jobId, searchQueryId)
-        .then(job => {
-            return res.status(200).send(job);
-        })
-        .catch(reason => {
-            return res.status(reason.status).send({message: reason.message});
-        });
+        .then((job) => res.status(200).send(job))
+        .catch((reason) => res.status(reason.status).send({ message: reason.message }));
 };
